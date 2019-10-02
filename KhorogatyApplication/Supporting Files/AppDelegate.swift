cfae670ded6478e8c4f9cfab6470b67fed7fd4ed
Ahmed <hamoda9355@yahoo.com>
2019-10-02 10:59:49 +0200

    First Commit

---
+++ KhorogatyApplication/Supporting Files/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  KhorogatyApplication
+//
+//  Created by Ahmed Ibrahim on 10/2/19.
+//  Copyright © 2019 Ahmed Ibrahim. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
